66144b41c8eaf1490872c7b6c52c5bc3478466ff
PSpeaking <137413264+PSpeaking@users.noreply.github.com>
2023-06-23 09:11:18 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 function checkVerbs() {
-    var verb1 = document.getElementById('verb1').value.toLowerCase();
-    // Add code to retrieve other input values
-  
-    if (verb1 === 'speaks' && verb2 === 'does not indicate' && verb3 === 'emphasized' && verb4 === 'was traveling' && verb5 === 'encourages') {
-      document.getElementById('feedback2').innerHTML = 'Correct!';
-    } else {
-      document.getElementById('feedback2').innerHTML = 'Incorrect. Try again.';
-    }
+  var verb1 = document.getElementById('verb1').value.toLowerCase();
+  var verb2 = document.getElementById('verb2').value.toLowerCase();
+  var verb3 = document.getElementById('verb3').value.toLowerCase();
+  var verb4 = document.getElementById('verb4').value.toLowerCase();
+  var verb5 = document.getElementById('verb5').value.toLowerCase();
+
+  if (verb1 === 'speaks' && verb2 === 'does not indicate' && verb3 === 'emphasized' && verb4 === 'was traveling' && verb5 === 'encourages') {
+    document.getElementById('feedback2').innerHTML = 'Correct!';
+  } else {
+    document.getElementById('feedback2').innerHTML = 'Incorrect. Try again.';
   }
+}
